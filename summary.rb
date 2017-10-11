860f99283fcb80369fe0834f95395fa9d497fb80
Thach <pcngocthach@gmail.com>
2017-10-11 22:27:11 +0700

    Example create migration article model

---
+++ summary.rb
@@ -0,0 +1,11 @@
+# Cơ bản ruby 
+# Tạo project mới: 
+#	rails new [ten_project]
+# Tạo một migrate table mới
+#	rails generate migration create_articales
+# Thực hiện migration 
+#	rake db:migrate
+# Hoàn tác migration
+#	rake db:rollback
+# Modify migration 
+#	rails generate migration add_description_to_articles
